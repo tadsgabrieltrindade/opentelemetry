5d4a151a6a6f53e44b148d94c9f8444c110b8ee5
Douglas Martins <douglimadasilva@gmail.com>
2025-08-02 17:33:48 -0300

    aula-5

--- aula-5/business/index.js
+++ aula-5/business/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { Pool } = require('pg');
 const logger = require('./logger');
+const api = require('@opentelemetry/api');
 
 const app = express();
 const port = 8082;
+
+const tracer = api.trace.getTracer('business-tracer');
 
 const pool = new Pool({
   user: 'user',
@@ -17,6 +20,20 @@ app.get('/user-details/:id', async (req, res) => {
   try {
     const { id } = req.params;
     logger.info({ userId: id }, `Fazendo uma requisição no PostgreSQL`)
+    
+    await tracer.startActiveSpan('validando-permissoes', async (span) => {
+      logger.info('Iniciando validação de permissões no banco de dados');
+
+      await new Promise(resolve => setTimeout(resolve, 50));
+
+      span.setAttribute('app.permission.level', 'admin');
+      span.addEvent('Permissões validadas com sucesso');
+
+      logger.info('Validação de permissões concluída');
+
+      span.end();
+    });
+    
     const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
     if (result.rows.length === 0) {
       return res.status(404).send('Usuário não encontrado');
